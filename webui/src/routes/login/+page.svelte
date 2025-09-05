9f61b0b7a937ef2894fbbb8a11a3c02fb375a3a9
Marvin Drees <marvin.drees@9elements.com>
2025-09-05 19:25:53 +0200

    feat(webui): add initial webui code
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

---
+++ webui/src/routes/login/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import LoginForm from '$lib/components/login-form.svelte';
+</script>
+
+<div class="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+	<div class="w-full max-w-sm md:max-w-3xl">
+		<LoginForm />
+	</div>
+</div>
