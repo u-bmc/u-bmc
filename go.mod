27b4640e60e5d53ed9d5efd17d2fa1a2c3cfecfe
Marvin Drees <marvin.drees@9elements.com>
2025-09-29 16:44:30 +0200

    feat: add thermalmgr
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/samber/slog-multi v1.5.0
 	github.com/samber/slog-zerolog/v2 v2.7.3
 	github.com/warthog618/go-gpiocdev v0.9.1
+	go.einride.tech/pid v0.1.3
 	go.opentelemetry.io/contrib/bridges/otelslog v0.13.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.63.0
 	go.opentelemetry.io/otel v1.38.0
--- service/thermalmgr/config.go
+++ service/thermalmgr/config.go
@@ -2,24 +2,574 @@
 
 package thermalmgr
 
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+const (
+	DefaultServiceName            = "thermalmgr"
+	DefaultServiceDescription     = "Thermal management service for BMC components"
+	DefaultServiceVersion         = "1.0.0"
+	DefaultThermalControlInterval = 2 * time.Second
+	DefaultEmergencyCheckInterval = 500 * time.Millisecond
+	DefaultDefaultPIDSampleTime   = 1 * time.Second
+	DefaultMaxThermalZones        = 16
+	DefaultMaxCoolingDevices      = 64
+	DefaultHwmonPath              = "/sys/class/hwmon"
+	DefaultDefaultWarningTemp     = 75.0
+	DefaultDefaultCriticalTemp    = 85.0
+	DefaultEmergencyShutdownTemp  = 95.0
+	DefaultDefaultPIDKp           = 1.0
+	DefaultDefaultPIDKi           = 0.1
+	DefaultDefaultPIDKd           = 0.05
+	DefaultDefaultOutputMin       = 0.0
+	DefaultDefaultOutputMax       = 100.0
+	DefaultEmergencyResponseDelay = 2 * time.Second
+	DefaultFailsafeCoolingLevel   = 100.0
+)
+
 type config struct {
-	name string
+	serviceName             string
+	serviceDescription      string
+	serviceVersion          string
+	enableThermalControl    bool
+	thermalControlInterval  time.Duration
+	emergencyCheckInterval  time.Duration
+	defaultPIDSampleTime    time.Duration
+	maxThermalZones         int
+	maxCoolingDevices       int
+	hwmonPath               string
+	enableHwmonDiscovery    bool
+	defaultWarningTemp      float64
+	defaultCriticalTemp     float64
+	emergencyShutdownTemp   float64
+	defaultPIDKp            float64
+	defaultPIDKi            float64
+	defaultPIDKd            float64
+	defaultOutputMin        float64
+	defaultOutputMax        float64
+	sensormonEndpoint       string
+	powermgrEndpoint        string
+	enableSensorIntegration bool
+	enablePowerIntegration  bool
+	persistThermalData      bool
+	streamName              string
+	streamSubjects          []string
+	streamRetention         time.Duration
+	enableEmergencyResponse bool
+	emergencyResponseDelay  time.Duration
+	failsafeCoolingLevel    float64
+	enableMetrics           bool
+	enableTracing           bool
 }
 
 type Option interface {
 	apply(*config)
 }
 
-type nameOption struct {
+type serviceNameOption struct {
 	name string
 }
 
-func (o *nameOption) apply(c *config) {
-	c.name = o.name
+func (o *serviceNameOption) apply(c *config) {
+	c.serviceName = o.name
 }
 
-func WithName(name string) Option {
-	return &nameOption{
-		name: name,
+func WithServiceName(name string) Option {
+	return &serviceNameOption{name: name}
+}
+
+type serviceDescriptionOption struct {
+	description string
+}
+
+func (o *serviceDescriptionOption) apply(c *config) {
+	c.serviceDescription = o.description
+}
+
+func WithServiceDescription(description string) Option {
+	return &serviceDescriptionOption{description: description}
+}
+
+type serviceVersionOption struct {
+	version string
+}
+
+func (o *serviceVersionOption) apply(c *config) {
+	c.serviceVersion = o.version
+}
+
+func WithServiceVersion(version string) Option {
+	return &serviceVersionOption{version: version}
+}
+
+type enableThermalControlOption struct {
+	enable bool
+}
+
+func (o *enableThermalControlOption) apply(c *config) {
+	c.enableThermalControl = o.enable
+}
+
+func WithThermalControl(enable bool) Option {
+	return &enableThermalControlOption{enable: enable}
+}
+
+func WithoutThermalControl() Option {
+	return &enableThermalControlOption{enable: false}
+}
+
+type thermalControlIntervalOption struct {
+	interval time.Duration
+}
+
+func (o *thermalControlIntervalOption) apply(c *config) {
+	c.thermalControlInterval = o.interval
+}
+
+func WithThermalControlInterval(interval time.Duration) Option {
+	return &thermalControlIntervalOption{interval: interval}
+}
+
+type emergencyCheckIntervalOption struct {
+	interval time.Duration
+}
+
+func (o *emergencyCheckIntervalOption) apply(c *config) {
+	c.emergencyCheckInterval = o.interval
+}
+
+func WithEmergencyCheckInterval(interval time.Duration) Option {
+	return &emergencyCheckIntervalOption{interval: interval}
+}
+
+type defaultPIDSampleTimeOption struct {
+	sampleTime time.Duration
+}
+
+func (o *defaultPIDSampleTimeOption) apply(c *config) {
+	c.defaultPIDSampleTime = o.sampleTime
+}
+
+func WithDefaultPIDSampleTime(sampleTime time.Duration) Option {
+	return &defaultPIDSampleTimeOption{sampleTime: sampleTime}
+}
+
+type maxThermalZonesOption struct {
+	maxVal int
+}
+
+func (o *maxThermalZonesOption) apply(c *config) {
+	c.maxThermalZones = o.maxVal
+}
+
+func WithMaxThermalZones(maxVal int) Option {
+	return &maxThermalZonesOption{maxVal: maxVal}
+}
+
+type maxCoolingDevicesOption struct {
+	maxVal int
+}
+
+func (o *maxCoolingDevicesOption) apply(c *config) {
+	c.maxCoolingDevices = o.maxVal
+}
+
+func WithMaxCoolingDevices(maxVal int) Option {
+	return &maxCoolingDevicesOption{maxVal: maxVal}
+}
+
+type hwmonPathOption struct {
+	path string
+}
+
+func (o *hwmonPathOption) apply(c *config) {
+	c.hwmonPath = o.path
+}
+
+func WithHwmonPath(path string) Option {
+	return &hwmonPathOption{path: path}
+}
+
+type enableHwmonDiscoveryOption struct {
+	enable bool
+}
+
+func (o *enableHwmonDiscoveryOption) apply(c *config) {
+	c.enableHwmonDiscovery = o.enable
+}
+
+func WithDiscovery(enable bool) Option {
+	return &enableHwmonDiscoveryOption{enable: enable}
+}
+
+func WithoutDiscovery() Option {
+	return &enableHwmonDiscoveryOption{enable: false}
+}
+
+type temperatureThresholdsOption struct {
+	warning   float64
+	critical  float64
+	emergency float64
+}
+
+func (o *temperatureThresholdsOption) apply(c *config) {
+	c.defaultWarningTemp = o.warning
+	c.defaultCriticalTemp = o.critical
+	c.emergencyShutdownTemp = o.emergency
+}
+
+func WithTemperatureThresholds(warning, critical, emergency float64) Option {
+	return &temperatureThresholdsOption{warning: warning, critical: critical, emergency: emergency}
+}
+
+type defaultPIDConfigOption struct {
+	kp float64
+	ki float64
+	kd float64
+}
+
+func (o *defaultPIDConfigOption) apply(c *config) {
+	c.defaultPIDKp = o.kp
+	c.defaultPIDKi = o.ki
+	c.defaultPIDKd = o.kd
+}
+
+func WithDefaultPIDConfig(kp, ki, kd float64) Option {
+	return &defaultPIDConfigOption{kp: kp, ki: ki, kd: kd}
+}
+
+type outputRangeOption struct {
+	minVal float64
+	maxVal float64
+}
+
+func (o *outputRangeOption) apply(c *config) {
+	c.defaultOutputMin = o.minVal
+	c.defaultOutputMax = o.maxVal
+}
+
+func WithOutputRange(minVal, maxVal float64) Option {
+	return &outputRangeOption{minVal: minVal, maxVal: maxVal}
+}
+
+type sensormonEndpointOption struct {
+	endpoint string
+}
+
+func (o *sensormonEndpointOption) apply(c *config) {
+	c.sensormonEndpoint = o.endpoint
+}
+
+func WithSensormonEndpoint(endpoint string) Option {
+	return &sensormonEndpointOption{endpoint: endpoint}
+}
+
+type powermgrEndpointOption struct {
+	endpoint string
+}
+
+func (o *powermgrEndpointOption) apply(c *config) {
+	c.powermgrEndpoint = o.endpoint
+}
+
+func WithPowermgrEndpoint(endpoint string) Option {
+	return &powermgrEndpointOption{endpoint: endpoint}
+}
+
+type enableSensorIntegrationOption struct {
+	enable bool
+}
+
+func (o *enableSensorIntegrationOption) apply(c *config) {
+	c.enableSensorIntegration = o.enable
+}
+
+func WithSensorIntegration(enable bool) Option {
+	return &enableSensorIntegrationOption{enable: enable}
+}
+
+func WithoutSensorIntegration() Option {
+	return &enableSensorIntegrationOption{enable: false}
+}
+
+type enablePowerIntegrationOption struct {
+	enable bool
+}
+
+func (o *enablePowerIntegrationOption) apply(c *config) {
+	c.enablePowerIntegration = o.enable
+}
+
+func WithPowerIntegration(enable bool) Option {
+	return &enablePowerIntegrationOption{enable: enable}
+}
+
+func WithoutPowerIntegration() Option {
+	return &enablePowerIntegrationOption{enable: false}
+}
+
+type integrationOption struct {
+	enableSensor bool
+	enablePower  bool
+}
+
+func (o *integrationOption) apply(c *config) {
+	c.enableSensorIntegration = o.enableSensor
+	c.enablePowerIntegration = o.enablePower
+}
+
+func WithIntegration(enableSensor, enablePower bool) Option {
+	return &integrationOption{enableSensor: enableSensor, enablePower: enablePower}
+}
+
+type persistThermalDataOption struct {
+	enable bool
+}
+
+func (o *persistThermalDataOption) apply(c *config) {
+	c.persistThermalData = o.enable
+}
+
+func WithPersistThermalData(enable bool) Option {
+	return &persistThermalDataOption{enable: enable}
+}
+
+func WithoutPersistThermalData() Option {
+	return &persistThermalDataOption{enable: false}
+}
+
+type streamNameOption struct {
+	name string
+}
+
+func (o *streamNameOption) apply(c *config) {
+	c.streamName = o.name
+}
+
+func WithStreamName(name string) Option {
+	return &streamNameOption{name: name}
+}
+
+type streamSubjectsOption struct {
+	subjects []string
+}
+
+func (o *streamSubjectsOption) apply(c *config) {
+	c.streamSubjects = o.subjects
+}
+
+func WithStreamSubjects(subjects ...string) Option {
+	set := make(map[string]struct{}, len(subjects))
+	out := make([]string, 0, len(subjects))
+	for _, s := range subjects {
+		s = strings.TrimSpace(s)
+		if s == "" {
+			continue
+		}
+		if _, ok := set[s]; ok {
+			continue
+		}
+		set[s] = struct{}{}
+		out = append(out, s)
 	}
+	return &streamSubjectsOption{subjects: out}
+}
+
+type streamRetentionOption struct {
+	retention time.Duration
+}
+
+func (o *streamRetentionOption) apply(c *config) {
+	c.streamRetention = o.retention
+}
+
+func WithStreamRetention(retention time.Duration) Option {
+	return &streamRetentionOption{retention: retention}
+}
+
+type persistenceOption struct {
+	enabled    bool
+	streamName string
+	retention  time.Duration
+}
+
+func (o *persistenceOption) apply(c *config) {
+	c.persistThermalData = o.enabled
+	c.streamName = o.streamName
+	c.streamRetention = o.retention
+}
+
+func WithPersistence(enabled bool, streamName string, retention time.Duration) Option {
+	return &persistenceOption{enabled: enabled, streamName: streamName, retention: retention}
+}
+
+type enableEmergencyResponseOption struct {
+	enable bool
+}
+
+func (o *enableEmergencyResponseOption) apply(c *config) {
+	c.enableEmergencyResponse = o.enable
+}
+
+func WithEmergencyResponse(enable bool) Option {
+	return &enableEmergencyResponseOption{enable: enable}
+}
+
+func WithoutEmergencyResponse() Option {
+	return &enableEmergencyResponseOption{enable: false}
+}
+
+type emergencyResponseDelayOption struct {
+	delay time.Duration
+}
+
+func (o *emergencyResponseDelayOption) apply(c *config) {
+	c.emergencyResponseDelay = o.delay
+}
+
+func WithEmergencyResponseDelay(delay time.Duration) Option {
+	return &emergencyResponseDelayOption{delay: delay}
+}
+
+type failsafeCoolingLevelOption struct {
+	level float64
+}
+
+func (o *failsafeCoolingLevelOption) apply(c *config) {
+	c.failsafeCoolingLevel = o.level
+}
+
+func WithFailsafeCoolingLevel(level float64) Option {
+	return &failsafeCoolingLevelOption{level: level}
+}
+
+type emergencyResponseConfigOption struct {
+	enabled       bool
+	delay         time.Duration
+	failsafeLevel float64
+}
+
+func (o *emergencyResponseConfigOption) apply(c *config) {
+	c.enableEmergencyResponse = o.enabled
+	c.emergencyResponseDelay = o.delay
+	c.failsafeCoolingLevel = o.failsafeLevel
+}
+
+func WithEmergencyResponseConfig(enabled bool, delay time.Duration, failsafeLevel float64) Option {
+	return &emergencyResponseConfigOption{enabled: enabled, delay: delay, failsafeLevel: failsafeLevel}
+}
+
+type enableMetricsOption struct {
+	enable bool
+}
+
+func (o *enableMetricsOption) apply(c *config) {
+	c.enableMetrics = o.enable
+}
+
+func WithMetrics(enable bool) Option {
+	return &enableMetricsOption{enable: enable}
+}
+
+func WithoutMetrics() Option {
+	return &enableMetricsOption{enable: false}
+}
+
+type enableTracingOption struct {
+	enable bool
+}
+
+func (o *enableTracingOption) apply(c *config) {
+	c.enableTracing = o.enable
+}
+
+func WithTracing(enable bool) Option {
+	return &enableTracingOption{enable: enable}
+}
+
+func WithoutTracing() Option {
+	return &enableTracingOption{enable: false}
+}
+
+func (c *config) Validate() error {
+	if c.serviceName == "" {
+		return fmt.Errorf("%w: service name cannot be empty", ErrInvalidConfiguration)
+	}
+
+	if c.serviceVersion == "" {
+		return fmt.Errorf("%w: service version cannot be empty", ErrInvalidConfiguration)
+	}
+
+	if c.thermalControlInterval <= 0 {
+		return fmt.Errorf("%w: thermal control interval must be positive", ErrInvalidConfiguration)
+	}
+
+	if c.emergencyCheckInterval <= 0 {
+		return fmt.Errorf("%w: emergency check interval must be positive", ErrInvalidConfiguration)
+	}
+
+	if c.defaultPIDSampleTime <= 0 {
+		return fmt.Errorf("%w: PID sample time must be positive", ErrInvalidConfiguration)
+	}
+
+	if c.maxThermalZones <= 0 {
+		return fmt.Errorf("%w: max thermal zones must be positive", ErrInvalidConfiguration)
+	}
+
+	if c.maxCoolingDevices <= 0 {
+		return fmt.Errorf("%w: max cooling devices must be positive", ErrInvalidConfiguration)
+	}
+
+	if c.defaultWarningTemp >= c.defaultCriticalTemp {
+		return fmt.Errorf("%w: warning temperature must be less than critical temperature", ErrInvalidConfiguration)
+	}
+
+	if c.defaultCriticalTemp >= c.emergencyShutdownTemp {
+		return fmt.Errorf("%w: critical temperature must be less than emergency shutdown temperature", ErrInvalidConfiguration)
+	}
+
+	if c.defaultOutputMin >= c.defaultOutputMax {
+		return fmt.Errorf("%w: output minimum must be less than output maximum", ErrInvalidConfiguration)
+	}
+
+	if c.failsafeCoolingLevel < 0 || c.failsafeCoolingLevel > 100 {
+		return fmt.Errorf("%w: failsafe cooling level must be between 0 and 100", ErrInvalidConfiguration)
+	}
+
+	if c.persistThermalData {
+		if c.streamName == "" {
+			return fmt.Errorf("%w: stream name cannot be empty when thermal data persistence is enabled", ErrInvalidConfiguration)
+		}
+
+		if len(c.streamSubjects) == 0 {
+			return fmt.Errorf("%w: at least one stream subject must be configured when thermal data persistence is enabled", ErrInvalidConfiguration)
+		}
+
+		for _, s := range c.streamSubjects {
+			if len(s) == 0 {
+				return fmt.Errorf("%w: stream subject cannot be empty", ErrInvalidConfiguration)
+			}
+		}
+
+		if c.streamRetention <= 0 {
+			return fmt.Errorf("%w: stream retention must be positive when thermal data persistence is enabled", ErrInvalidConfiguration)
+		}
+	}
+
+	if c.enableSensorIntegration && c.sensormonEndpoint == "" {
+		return fmt.Errorf("%w: sensormon endpoint cannot be empty when sensor integration is enabled", ErrInvalidConfiguration)
+	}
+
+	if c.enablePowerIntegration && c.powermgrEndpoint == "" {
+		return fmt.Errorf("%w: powermgr endpoint cannot be empty when power integration is enabled", ErrInvalidConfiguration)
+	}
+
+	if c.enableEmergencyResponse && c.emergencyResponseDelay <= 0 {
+		return fmt.Errorf("%w: emergency response delay must be positive when emergency response is enabled", ErrInvalidConfiguration)
+	}
+
+	return nil
 }
