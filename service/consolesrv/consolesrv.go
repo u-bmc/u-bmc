2cba7bbf96deefee0affce6f86b36218c91b82f7
Marvin Drees <marvin.drees@9elements.com>
2025-10-14 09:22:16 -0700

    feat(console): align config names
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

--- service/consolesrv/consolesrv.go
+++ service/consolesrv/consolesrv.go
@@ -13,10 +13,12 @@ import (
 // Compile-time assertion that ConsoleSrv implements service.Service.
 var _ service.Service = (*ConsoleSrv)(nil)
 
+// ConsoleSrv provides console server functionality for BMC access.
 type ConsoleSrv struct {
-	config
+	config config
 }
 
+// New creates a new ConsoleSrv instance with the provided options.
 func New(opts ...Option) *ConsoleSrv {
 	cfg := &config{
 		name: "consolesrv",
@@ -30,16 +32,16 @@ func New(opts ...Option) *ConsoleSrv {
 }
 
 func (s *ConsoleSrv) Name() string {
-	return s.name
+	return s.config.name
 }
 
 func (s *ConsoleSrv) Run(ctx context.Context, ipcConn nats.InProcessConnProvider) error {
 	l := log.GetGlobalLogger()
 
-	l.InfoContext(ctx, "Starting console server", "service", s.name)
+	l.InfoContext(ctx, "Starting console server", "service", s.config.name)
 
 	<-ctx.Done()
-	l.InfoContext(ctx, "Stopping console server", "service", s.name, "reason", ctx.Err())
+	l.InfoContext(ctx, "Stopping console server", "service", s.config.name, "reason", ctx.Err())
 
 	return ctx.Err()
 }
