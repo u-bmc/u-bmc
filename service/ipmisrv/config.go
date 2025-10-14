787529cd25e250f6b9f38bc6a3f2695396717bbf
Marvin Drees <marvin.drees@9elements.com>
2025-10-14 09:19:37 -0700

    feat(ipmi): align config names
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

--- service/ipmisrv/config.go
+++ service/ipmisrv/config.go
@@ -2,10 +2,12 @@
 
 package ipmisrv
 
+// config holds the configuration for the IPMI server service.
 type config struct {
 	name string
 }
 
+// Option represents a configuration option for the IPMI server service.
 type Option interface {
 	apply(*config)
 }
@@ -18,7 +20,7 @@ func (o *nameOption) apply(c *config) {
 	c.name = o.name
 }
 
-func WithName(name string) Option {
+func WithServiceName(name string) Option {
 	return &nameOption{
 		name: name,
 	}
